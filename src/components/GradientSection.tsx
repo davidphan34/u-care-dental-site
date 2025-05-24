c7f124d77f0fd426e7e02fd30506ff574873b84c
David Phan <davidphan34@gmail.com>
2025-05-23 18:44:00 -0700

    chore: update site visuals - adjusted colors, resized images, refined layouts

--- src/components/GradientSection.tsx
+++ src/components/GradientSection.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function GradientSection({ children }: { children: ReactNode }) {
   return (
-    <section className="min-h-[calc(100vh-88px)] bg-gradient-to-br from-indigo-100 via-white to-indigo-200 px-6 py-5">
+    <section className="min-h-[calc(100vh-88px)] bg-[#ddf2ed] px-6 py-5">
       {children}
     </section>
   );
